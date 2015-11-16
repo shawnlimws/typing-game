3c9653fc6fd5e880078b1321ddb5468d0e14dae5
Shawn Lim <shawnlim.ws@gmail.com>
2015-11-16 13:42:37 +0800

    checking words

--- game/game.js
+++ game/game.js
@@ -1,10 +1,26 @@
 var randomWords = require('random-words')
+var sharkOne = document.getElementById('sharkone')
+var sharkTwo = document.getElementById('sharktwo')
+var sharkThree = document.getElementById('sharkthree')
+var sharkFour = document.getElementById('sharkfour')
+var sharkFive = document.getElementById('sharkfive')
+var playerInput = document.getElementById('playertext')
+var sharks = Array.from(document.getElementsByClassName('sharks'))
 
-console.log(randomWords(1))
-
+// Create the shark words
 function generateWords () {
-  var wordAttack = document.createTextNode(randomWords(1))
-  document.getElementById('sharkone').appendChild(document.createTextNode(randomWords(1)))
-  document.getElementById('sharktwo').appendChild(document.createTextNode(randomWords(1)))
+  sharks.forEach(shark =>
+    shark.appendChild(
+      document.createTextNode(randomWords(1))
+    )
+  )
 }
 generateWords()
+
+// Check words
+document.querySelector('input').focus()
+document.getElementById('playerField').addEventListener('input', checkWords)
+
+function checkWords (event) {
+  if (event.target === sharkOne
+}
