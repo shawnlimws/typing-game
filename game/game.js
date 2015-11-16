7dadb62dfcc954519f8d57de14da7892303e190f
Shawn Lim <shawnlim.ws@gmail.com>
2015-11-16 15:39:07 +0800

    made input field

--- game/game.js
+++ game/game.js
@@ -6,6 +6,7 @@ var sharkFour = document.getElementById('sharkfour')
 var sharkFive = document.getElementById('sharkfive')
 var playerInput = document.getElementById('playertext')
 var sharks = Array.from(document.getElementsByClassName('sharks'))
+var inputField = document.getElementById('playerField')
 
 // Create the shark words
 function generateWords () {
@@ -18,9 +19,26 @@ function generateWords () {
 generateWords()
 
 // Check words
-document.querySelector('input').focus()
+document.querySelector('input').autofocus = true
 document.getElementById('playerField').addEventListener('input', checkWords)
 
 function checkWords (event) {
-  if (event.target === sharkOne
+  if (event.target.value === sharkOne.innerText) {
+    sharkOne.className = 'shark-beaten'
+  } else if (event.target.value === sharkTwo.innerText) {
+    sharkTwo.className = 'shark-beaten'
+  } else if (event.target.value === sharkThree.innerText) {
+    sharkThree.className = 'shark-beaten'
+  } else if (event.target.value === sharkFour.innerText) {
+    sharkFour.className = 'shark-beaten'
+  } else if (event.target.value === sharkFive.innerText) {
+    sharkFive.className = 'shark-beaten'
+  }
 }
+
+// Clear input box
+inputField.addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    inputField.value = ''
+  }
+})
