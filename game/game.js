28e4191eb97cc7a293ad83c4a6131750a9de1907
Shawn Lim <shawnlim.ws@gmail.com>
2015-11-18 00:03:57 +0800

    removed animation

--- game/game.js
+++ game/game.js
@@ -2,6 +2,7 @@ var randomWords = require('random-words')
 var playerInput = document.getElementById('playertext')
 var sharks = Array.from(document.getElementsByClassName('sharks'))
 var inputField = document.getElementById('playerField')
+var sharkOne = document.getElementById('sharkone')
 
 // Create the shark words
 function generateWords () {
@@ -22,11 +23,15 @@ function checkWords (event) {
   sharks.forEach(shark => {
     if (shark.innerText === inputField.value && event.keyCode === 13) {
       shark.style.backgroundImage = 'url(../injured-shark.png)'
-      shark.style.animationPlayState = 'paused'
       inputField.value = ''
-    } else return
+    }
+  })
+  if (gameOver(sharks)) {
+    sharks.forEach(shark => {
+      shark.className = ''
+    })
   }
-) }
+}
 
 // Clear the input box
 function clearField (event) {
@@ -35,4 +40,17 @@ function clearField (event) {
   }
 }
 
-// Make the images move
+function gameOver (sharks) {
+  return sharks
+    .every(shark => shark.style.animationPlayState === 'paused')
+}
+// Animate sharks
+function moveShark (shark) {
+  sharkOne.style.left = parseInt(sharkOne) + 10 + 'px'
+  Animate = setTimeout(moveShark, 20)
+}
+// things to do:
+// 1) add the filter function and split up the checkWords function
+// 2) change the input to a form so don't have to requure keyCode 13
+// 3) can check if animation is paused then restart game
+// 4) need to add animation into javascript, so that can call on it to recreate words
