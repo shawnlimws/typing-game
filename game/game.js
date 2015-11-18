797ded47f3d39e495b9a0d83ed1cb655a9133604
Shawn Lim <shawnlim.ws@gmail.com>
2015-11-18 14:19:30 +0800

    added back animation

--- game/game.js
+++ game/game.js
@@ -1,8 +1,6 @@
 var randomWords = require('random-words')
-var playerInput = document.getElementById('playertext')
 var sharks = Array.from(document.getElementsByClassName('sharks'))
 var inputField = document.getElementById('playerField')
-var sharkOne = document.getElementById('sharkone')
 
 // Create the shark words
 function generateWords () {
@@ -24,13 +22,9 @@ function checkWords (event) {
     if (shark.innerText === inputField.value && event.keyCode === 13) {
       shark.style.backgroundImage = 'url(../media/injured-shark.png)'
       inputField.value = ''
+      shark.style.animationPlayState = 'paused'
     }
   })
-  if (gameOver(sharks)) {
-    sharks.forEach(shark => {
-      shark.className = ''
-    })
-  }
 }
 
 // Clear the input box
@@ -40,17 +34,10 @@ function clearField (event) {
   }
 }
 
-function gameOver (sharks) {
-  return sharks
-    .every(shark => shark.style.animationPlayState === 'paused')
-}
-// Animate sharks
-function moveShark (shark) {
-  sharkOne.style.left = parseInt(sharkOne) + 10 + 'px'
-  Animate = setTimeout(moveShark, 20)
-}
 // things to do:
 // 1) add the filter function and split up the checkWords function
 // 2) change the input to a form so don't have to requure keyCode 13
 // 3) can check if animation is paused then restart game
-// 4) need to add animation into javascript, so that can call on it to recreate words
+
+// questions:
+// 1) why is vertical not responsive
